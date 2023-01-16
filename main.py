1528db84192b556d9edd007e61cea06d2327dda9
Aditya-Manoj <adityamanoj19@gmail.com>
2023-01-16 17:25:51 +0530

    This is a local Commit

--- main.py
+++ main.py
@@ -10,3 +10,7 @@ print("EDITED on GITHUB")
 
 # extra addition to check PULL command
 print("PULL command is working")
+
+
+# Creating a local commit in git 
+print("This is for checking LOCAL COMMIT PUSH")
